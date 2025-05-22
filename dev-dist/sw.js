e08a46cb117276eccfd6676b127d40b0d6c91bea
Omotola Oyeniyi <kingsx@localhost.localdomain>
2025-05-22 22:16:56 +0100

    Replace with backup version

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "93c03271ce1b8e767439fa60c585d12d"
   }, {
     "url": "index.html",
-    "revision": "0.ucnft5k9sj8"
+    "revision": "0.ccb5ggeqoe8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
