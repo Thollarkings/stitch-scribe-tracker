a846d1cc0bbfd731dcebc14949c2332e793ce3d5
Omotola Oyeniyi <kingsx@localhost.localdomain>
2025-05-23 15:37:57 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/stitch-scribe-tracker/sw.js', { scope: '/stitch-scribe-tracker/' })})}
